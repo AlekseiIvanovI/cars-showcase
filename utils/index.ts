25c81e92102a2bbb4348ff3e1c90624ca54f967c
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2023-11-27 21:00:22 -0800

    new commit

--- utils/index.ts
+++ utils/index.ts
@@ -11,10 +11,10 @@ export async function fetchCars(filters: FilterProps) {
         const responce = await 
         fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
             headers: headers,
+            
         });
 
         const result = await responce.json();
-
         return result;
     }
 
