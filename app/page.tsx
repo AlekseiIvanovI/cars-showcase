5d5ca07690570bb42710759e76e9de565dd9e2c2
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2023-12-23 22:46:58 -0800

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,131 +1,192 @@
 "use client"
 
+import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
+import { fuels, yearsOfProduction } from "@/constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
-//DONT DELETE
-import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
-import { fuels, yearsOfProduction } from '@/constants';
-import { fetchCars } from '@/utils';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-
-//server side approach
-//export default async function Home({ searchParams }) {
-
-export default function Home() {
-  //server side approach
-  // const allCars = await fetchCars({
-  //   manufacturer: searchParams.manufacturer || '',
-  //   year: searchParams.year || 2022,
-  //   fuel: searchParams.fuel || '',
-  //   limit: searchParams.limit || 10,
-  //   model: searchParams.model || '',
-  // });
-
-  const [allCars, setAllCars] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  //search states
-  const [manufacturer, setManufacturer] = useState("")
-  const [model, setModel] = useState("");
-
-  //filter states
-  const [fuel, setFuel] = useState("");
-
-  const [year, setYear] = useState(2022);
-
-  //pagination state
-  const [limit, setLimit] = useState(10);
-
-  const getCars = async () => {
-    setLoading(true);
-    try{
-      const result = await fetchCars({
-        manufacturer: manufacturer || '',
-        year: year || 2022,
-        fuel: fuel || '',
-        limit: limit || 10,
-        model: model || '',
-      });
-
-      setAllCars(result);
-    }catch(error){
-      console.log(error);
-    }finally{
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    console.log(fuel, year, limit, model)
-      getCars();
-  }, [manufacturer, model, fuel, year, limit])
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
-
   return (
-    <main 
-    className="overflow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
-      <div className='mt-12 paddding-x padding-y max-width id="discover'>
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore the cars you might like</p>
+          <p>Explore out cars you might like</p>
         </div>
+
         <div className='home__filters'>
-          <SearchBar setManufacturer={setManufacturer}
-            setModel={setModel}
-          />
+          <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter title="fuel" options={fuels}
-            // @ts-ignore
-            setFilter={setFuel}
-
-            />
-            <CustomFilter title="year" options={yearsOfProduction}
-            // @ts-ignore
-            setFilter={setYear}
-            />
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
-          {allCars.length > 0 ? (
-            <section>
-              <div className='home__cars-wrapper'>
-                {allCars?.map((car) => (
-                <CarCard car={car}/>
-                ))}
-              </div>
 
-                  {loading && (
-                    <div className='mt-16 w-full flex-center'>
-                      <Image 
-                      src="/loader.svg"
-                      alt="Loader"
-                      width={50}
-                      height={50}
-                      className="object-contain"
-                      />
-                    </div>
-                  )}
-
-              <ShowMore 
-                pageNumber={limit / 10}
-                isNext={limit > allCars.length}
-                // @ts-ignore
-                setLimit={setLimit}
-              />
-            </section>
-          ):(
-            <div className='home__error-container'
-            >
-              <h2 className='text-black text-xl 
-              font-bold'>Oops, no results</h2>
-              <p>{allCars?.message}</p>
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
             </div>
-          )}
 
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
-  )
+  );
 }
+
+// "use client"
+
+
+// //DONT DELETE
+// import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
+// import { fuels, yearsOfProduction } from '@/constants';
+// import { fetchCars } from '@/utils';
+// import Image from 'next/image';
+// import { useEffect, useState } from 'react';
+
+// //server side approach
+// //export default async function Home({ searchParams }) {
+
+// export default function Home() {
+//   //server side approach
+//   // const allCars = await fetchCars({
+//   //   manufacturer: searchParams.manufacturer || '',
+//   //   year: searchParams.year || 2022,
+//   //   fuel: searchParams.fuel || '',
+//   //   limit: searchParams.limit || 10,
+//   //   model: searchParams.model || '',
+//   // });
+
+//   const [allCars, setAllCars] = useState([]);
+//   const [loading, setLoading] = useState(false);
+
+//   //search states
+//   const [manufacturer, setManufacturer] = useState("")
+//   const [model, setModel] = useState("");
+
+//   //filter states
+//   const [fuel, setFuel] = useState("");
+
+//   const [year, setYear] = useState(2022);
+
+//   //pagination state
+//   const [limit, setLimit] = useState(10);
+
+//   const getCars = async () => {
+//     setLoading(true);
+//     try{
+//       const result = await fetchCars({
+//         manufacturer: manufacturer || '',
+//         year: year || 2022,
+//         fuel: fuel || '',
+//         limit: limit || 10,
+//         model: model || '',
+//       });
+
+//       setAllCars(result);
+//     }catch(error){
+//       console.log(error);
+//     }finally{
+//       setLoading(false);
+//     }
+//   }
+
+//   useEffect(() => {
+//     console.log(fuel, year, limit, model)
+//       getCars();
+//   }, [manufacturer, model, fuel, year, limit])
+
+//   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+
+//   return (
+//     <main 
+//     className="overflow-hidden">
+//       <Hero />
+//       <div className='mt-12 paddding-x padding-y max-width id="discover'>
+//         <div className='home__text-container'>
+//           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+//           <p>Explore the cars you might like</p>
+//         </div>
+//         <div className='home__filters'>
+//           <SearchBar setManufacturer={setManufacturer}
+//             setModel={setModel}
+//           />
+
+//           <div className='home__filter-container'>
+//             <CustomFilter title="fuel" options={fuels}
+//             // @ts-ignore
+//             setFilter={setFuel}
+
+//             />
+//             <CustomFilter title="year" options={yearsOfProduction}
+//             // @ts-ignore
+//             setFilter={setYear}
+//             />
+//           </div>
+//         </div>
+//           {allCars.length > 0 ? (
+//             <section>
+//               <div className='home__cars-wrapper'>
+//                 {allCars?.map((car) => (
+//                 <CarCard car={car}/>
+//                 ))}
+//               </div>
+
+//                   {loading && (
+//                     <div className='mt-16 w-full flex-center'>
+//                       <Image 
+//                       src="/loader.svg"
+//                       alt="Loader"
+//                       width={50}
+//                       height={50}
+//                       className="object-contain"
+//                       />
+//                     </div>
+//                   )}
+
+//               <ShowMore 
+//                 pageNumber={limit / 10}
+//                 isNext={limit > allCars.length}
+//                 // @ts-ignore
+//                 setLimit={setLimit}
+//               />
+//             </section>
+//           ):(
+//             <div className='home__error-container'
+//             >
+//               <h2 className='text-black text-xl 
+//               font-bold'>Oops, no results</h2>
+//               <p>{allCars?.message}</p>
+//             </div>
+//           )}
+
+//       </div>
+//     </main>
+//   )
+// }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import React, { useState } from "react"
-
-import { SearchManufacturer } from "./"
-import Image from "next/image"
+import Image from "next/image";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
@@ -18,55 +18,76 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   </button>
 );
 
-const SearchBar = ( { setManufacturer, setModel }) => {
-    const [searchManufacturer, setSearchManufacturer] = useState('');
-    const [searchModel, setSearchModel] = useState('');
-    const router = useRouter();
+const SearchBar = () => {
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
 
-    const handleSearch = (e: React.
-      FormEvent<HTMLFormElement>) =>{
-        e.preventDefault();
+  const router = useRouter();
 
-        if(searchManufacturer === '' && searchModel === '') {
-        return alert('Please fill in the car make/model search bar');
-        }
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        setModel(searchModel), 
-        setManufacturer(searchManufacturer)
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
     }
 
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
 
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    // Create a new URLSearchParams object using the current URL search parameters
+    const searchParams = new URLSearchParams(window.location.search);
+
+    // Update or delete the 'model' search parameter based on the 'model' value
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+       searchParams.delete("manufacturer");
+    }
+
+    // Generate the new pathname with the updated search parameters
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
-        <div className="searchbar__item">
-            <SearchManufacturer
-                selected={searchManufacturer}
-                setSelected={setSearchManufacturer}
-            />
-            <SearchButton otherClasses="sm:hidden"/>
-        </div>
-        <div className="searchbar__item">
-          <Image 
-            src="/model-icon.png"
-            width={25}
-            height={25}
-            className="absolute w-[20px] h-[20px] ml-4"
-            alt="car model"
-          />
-          <input
-          type="text"
-          name="model"
-          value={searchModel}
-          onChange={(e) => setSearchModel(e.target.value)}
-          placeholder="Search for model"
-          className="searchbar__input"
-          />
-          <SearchButton otherClasses="sm:hidden"/>
-        </div>
-        <SearchButton otherClasses="max-sm:hidden"/>
+      <div className='searchbar__item'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManuFacturer}
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
